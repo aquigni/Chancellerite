32d27e20fba64776a6d83f950da899fdcfd5ac2c
Glitch <aquigni@users.noreply.github.com>
2023-11-24 22:55:31 +0300

    Corrected py script

--- transformations/py/transform_proverbs.py
+++ transformations/py/transform_proverbs.py
@@ -1,32 +1,30 @@
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
-import requests
+
+# Load environment variables from .env file
+load_dotenv()
+
+# API key, set it in .env (remember to add .env to .gitignore)
+client = OpenAI()
 
 def transform_proverb(proverb, index, total):
-    # Only print the counter
-    print(f"Processing {index}/{total}")
-    # API key, set it in .env (remember to add .env to .gitignore)
-    client = OpenAI()
+    print(f"Processing {index}/{total}")  # Only print the counter
 
-    headers = {"Authorization": f"Bearer {api_key}"}
-    data = {
-        "model": "gpt-4-1106-preview",
-        "messages": [{
-            "role": "system",
-            "content": "Вы работаете в роли переводчика, который переформулирует поговорки в максимально бюрократический канцеляритный стиль. Пример: «Цыплят по осени считают» превратится в «Подсчет прироста домашней птицы производится после завершения сезона сельскохозяйственных работ»."
-        }, {
-            "role": "user",
-            "content": f"Переформулируйте поговорку, без заключения её в кавычки и без печати в ответе исходной: '{proverb}'"
-        }]
-    }
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+                {"role": "system", "content": "Вы работаете в роли переводчика, который переформулирует поговорки в максимально бюрократический канцеляритный стиль. Пример: «Цыплят по осени считают» превратится в «Подсчет прироста домашней птицы производится после завершения сезона сельскохозяйственных работ»."},
+                {"role": "user", "content": f"Переформулируйте поговорку, без заключения её в кавычки и без печати в ответе исходной: '{proverb}'"}
+            ]
+        )
 
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-    response_json = response.json()
-
-    if response_json.get("choices"):
-        transformed = response_json.get("choices")[0].get("message").get("content").strip()
+        transformed = completion.choices[0].message.content.strip()
         return transformed
-    else:
-        print(f"Error with proverb '{proverb}':", response_json)
+
+    except Exception as e:
+        print(f"Error with proverb '{proverb}': {e}")
         return None
 
 # Reading proverbs from file
@@ -39,4 +37,7 @@ transformed_proverbs = [transform_proverb(proverb, index+1, total_proverbs) for 
 # Writing result to file
 with open("../txt/transformed_proverbs.txt", "w") as file:
     for proverb in transformed_proverbs:
-        file.write(proverb + "\n")
+        if proverb is not None:
+            file.write(proverb + "\n")
+        else:
+            file.write("Transformation Failed\n")
